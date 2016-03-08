5c88eaeec89301d5824014b2c412f1f47df4b6b7
Toma Luca <toma.luca95@gmail.com>
2016-03-08 17:10:09 +0100

    Updated gitignore and location of wtl.conf

--- const.sh
+++ const.sh
@@ -6,7 +6,11 @@ if [[ ! -f "$0" ]] ; then
 fi
 
 export WTL_DIR=$(pwd)
-export WTL_CONFIG_FILE=$WTL_DIR"/wtl.conf"
 export WTL_REPO_DIR=$WTL_DIR"/WikiToLearn"
 export WTL_CONFIGS_DIR=$WTL_DIR'/configs/'
 export WTL_ARCHIVES=$WTL_DIR"/archives/"
+
+export WTL_CONFIG_FILE=$WTL_CONFIGS_DIR"/wtl.conf"
+echo
+echo $WTL_CONFIG_FILE
+echo
