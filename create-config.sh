bb04d2e6de546e77f8c9cc4082006ecbfe0db338
Cristian Baldi <bld.cris.96@gmail.com>
2016-03-08 23:33:10 +0000

    Forgot create config

--- create-config.sh
+++ create-config.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [[ $(id -u) -eq 0 ]] || [[ $(id -g) -eq 0 ]] ; then
+ echo "You can't be root. root has too much power."
+ exit 1
+fi
+
 cd $(dirname $(realpath $0))
 if [[ ! -f "$0" ]] ; then
  echo "Error changing directory"
@@ -14,7 +20,7 @@ done
 
 if [[ -f "$WTL_CONFIG_FILE" ]] ; then
  echo "You have the '"$WTL_CONFIG_FILE"' file on your directory"
- exit 1
+ exit 1a6f0e6f6298973f2df3b0c95c6321a21b2cb2d3f
 fi
 
 echo -n "You want to use ssh or https to clone the repository? (https or ssh) "
@@ -34,15 +40,33 @@ case "$proto" in
  ;;
 esac
 
-if [[ -d "$WTL_REPO_DIR" ]] ; then
- echo "You have '"$WTL_REPO_DIR"' directory in your directory."
- echo "If you are trying to re-create the config file move the directory way and re-execute this script"
- exit 1
-fi
+#if [[ -d "$WTL_REPO_DIR" ]] ; then
+# echo "You have '"$WTL_REPO_DIR"' directory in your directory."
+# echo "If you are trying to re-create the config file move the directory way and re-execute this script"
+# exit 1
+#fi
 
 if [[ "$WTL_INSTANCE_NAME" == "" ]] ; then
  echo "Using default instance name"
  export WTL_INSTANCE_NAME="WTL-dev"
+fi
+
+echo -n "GitHub Auth Token: "
+read WTL_GITHUB_TOKEN
+if [[ "$WTL_GITHUB_TOKEN" == "" ]] ; then
+ echo "A token is REQUIRED!"
+ exit 1
+else
+    cat <<EOF > $WTL_DIR/configs/composer/auth.json 
+{
+   "config":{
+      "github-oauth":{
+         "github.com":"$WTL_GITHUB_TOKEN"
+      }
+   }
+}
+EOF
+
 fi
 
 {
