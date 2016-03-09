117d301f1f0bfc2c61f31639a64505511380d8b5
DavideMaggiorelli <Davide.Maggiorelli@gmail.com>
2016-03-10 00:51:24 +0100

    scripts improvements

--- do-one-composer.sh
+++ do-one-composer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Install composer dependencies from composer.json in the $1 folder 
 
+#check existence of the directory WikiToLearn
 if [[ "$WTL_DIR" == "" ]] ; then
  echo "You can't run this script"
  exit 1
 fi
 
+#check if $1 is null
 if [ -z "$1" ]; then
     echo "Warning: required parameter 'location of composer.json'" 
     exit -1
@@ -13,17 +15,18 @@ fi
 
 echo $1
 
+#cd to current script folder
 cd $(dirname $(realpath $0))
 if [[ ! -f "$0" ]] ; then
  echo "Error changing directory"
  exit 1
 fi
 
+#call ./load-wikitolearn.sh script: load environment variables from const and config file
 . ./load-wikitolearn.sh
 
-if [[ -f "$WTL_DIR/configs/composer/auth.json" ]] ; then
-    echo "Composer config OK"
-else
+#check if auth.json is in the composer folder
+if [[ ! -f "$WTL_DIR/configs/composer/auth.json" ]] ; then
     echo "Composer config missing, please run create-config.sh"
     exit 1
 fi
