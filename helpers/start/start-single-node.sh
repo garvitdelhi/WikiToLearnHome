1eb55d3e71e22d81a6716574adefe01494af83af
DavideMaggiorelli <Davide.Maggiorelli@gmail.com>
2016-03-12 19:33:08 +0100

    dockers start scripts

--- helpers/start/start-single-node.sh
+++ helpers/start/start-single-node.sh
@@ -5,9 +5,29 @@ if [[ $? -ne 0 ]] ; then
  exit 1
 fi
 docker start ${WTL_INSTANCE_NAME}-parsoid
+if [[ $? -ne 0 ]] ; then
+ echo "FATAL ERROR: MISSING PARSOID"
+ exit 1
+fi
 docker start ${WTL_INSTANCE_NAME}-mathoid
+if [[ $? -ne 0 ]] ; then
+ echo "FATAL ERROR: MISSING PARSOID"
+ exit 1
+fi
 docker start ${WTL_INSTANCE_NAME}-memcached
+if [[ $? -ne 0 ]] ; then
+ echo "FATAL ERROR: MISSING PARSOID"
+ exit 1
+fi
 docker start ${WTL_INSTANCE_NAME}-mysql
+if [[ $? -ne 0 ]] ; then
+ echo "FATAL ERROR: MISSING PARSOID"
+ exit 1
+fi
 docker start ${WTL_INSTANCE_NAME}-parsoid:parsoid
+if [[ $? -ne 0 ]] ; then
+ echo "FATAL ERROR: MISSING PARSOID"
+ exit 1
+fi
 docker start ${WTL_INSTANCE_NAME}-websrv
 
