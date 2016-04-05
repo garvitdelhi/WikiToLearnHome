a76ecd156f8e4e9ef3b60a36f3405dd1e40b95b0
Toma Luca <toma.luca95@gmail.com>
2016-04-05 21:24:57 +0200

    Added update-docker-container to allow an easy update of docker images

--- instance.sh
+++ instance.sh
@@ -50,7 +50,17 @@ case $1 in
     delete-volumes)
         $WTL_SCRIPTS/delete-volumes.sh
     ;;
-    import-devdump)
+    update-docker-container)
+        $WTL_SCRIPTS/pull-images.sh
+        $WTL_SCRIPTS/unuse-instance.sh
+        $WTL_SCRIPTS/stop.sh
+        $WTL_SCRIPTS/delete.sh
+        $WTL_SCRIPTS/create.sh
+        $WTL_SCRIPTS/start.sh
+        $WTL_SCRIPTS/use-instance.sh
+        $WTL_SCRIPTS/fix-hosts.sh
+    ;;
+    devdump-import)
         $WTL_SCRIPTS/backup-restore.sh $WTL_REPO_DIR/DeveloperDump/
         $WTL_SCRIPTS/update-db.sh
     ;;
