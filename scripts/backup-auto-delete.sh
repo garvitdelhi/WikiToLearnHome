be5d31345d0e713d67dfcf374cef60f5827bc59b
Toma Luca <toma.luca95@gmail.com>
2016-04-06 00:29:08 +0200

    using log funcion

--- scripts/backup-auto-delete.sh
+++ scripts/backup-auto-delete.sh
@@ -16,14 +16,15 @@ fi
 . $WTL_SCRIPTS/environments/${WTL_ENV}.sh
 
 if [[ "$WTL_INSTANCE_NAME" == "" ]] ; then
- echo "You need the WTL_INSTANCE_NAME env"
- exit 1
+    wtl-log backup-auto-delete.sh 0 MIGGING_WTL_INSTANCE_NAME "You need the WTL_INSTANCE_NAME env"
+    exit 1
 fi
 
 
 
 while [[ $(ls $WTL_BACKUPS | grep -v StaticBackup | wc -l) -gt 1 ]] ; do
-    echo "Deleting the backup: "`ls -tr $WTL_BACKUPS | grep -v StaticBackup | head -1`
-    rm -Rf $WTL_BACKUPS"/"`ls -tr $WTL_BACKUPS | grep -v StaticBackup | head -1`"/"
+    BACKUP_TO_DELETE=`ls -tr $WTL_BACKUPS | grep -v StaticBackup | head -1`
+    wtl-log backup-auto-delete.sh 3 BACKUP_AUTO_DELETE "Deleting the backup: "$BACKUP_TO_DELETE
+    rm -Rf $WTL_BACKUPS"/"$BACKUP_TO_DELETE"/"
 done
 
