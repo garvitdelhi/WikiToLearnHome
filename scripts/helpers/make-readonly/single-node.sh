7c9ff17bd6fed661541cf63fc0ab8b6ffca006cf
Toma Luca <toma.luca95@gmail.com>
2016-04-05 22:21:59 +0200

    added script to update the developer dump fix minor bug on the readonly/readwrite scripts

--- scripts/helpers/make-readonly/single-node.sh
+++ scripts/helpers/make-readonly/single-node.sh
@@ -7,6 +7,9 @@ if [[ "$@" != "" ]] ; then
 fi
 
 UUID=$(echo $MSG | sha1sum | awk '{ print $1 }')
+if test ! -f $WTL_WORKING_DIR"/LocalSettings.d/wgReadOnly.php" ; then
+    touch $WTL_WORKING_DIR"/LocalSettings.d/wgReadOnly.php"
+fi
 
 grep UUID $WTL_WORKING_DIR"/LocalSettings.d/wgReadOnly.php" | grep $UUID &> /dev/null || {
 
