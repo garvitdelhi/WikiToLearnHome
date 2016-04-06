1490a8f11ff77ce3c0b5b183acae4d832493f386
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-04-06 21:44:30 +0200

    Beatiful output
    Signed-off-by: Davide Valsecchi <valsecchi.davide94@gmail.com>

--- scripts/git-gpg-check/check-last-commit-signature.sh
+++ scripts/git-gpg-check/check-last-commit-signature.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 #
-# Validate signatures on the last commit
+# This script checks the signature of the last commit.
+# If the gpg signature is trusted (inside gpg keyring)
+# the script prints the name of the author and returns 0.
 ##
 
-git log --pretty="format:%G?"  HEAD~1..HEAD | grep -v "G" &> /dev/null
+
+#reading author of last commit
+author=$(git log --pretty="format:%an %ae"  HEAD~1..HEAD)
+#checking signature
+trusted=$(git log --pretty="format:%G?"  HEAD~1..HEAD)
 
 # grep will exit with a non-zero status if no matches are found, which we
 # consider a success, so invert it
-if [[ $? -gt 0 ]]; then
-    echo "[git-gpg-check] Last commit is trusted..."
+if [[ $trusted == 'G' ]]; then
+    echo "[git-gpg-check] Last commit is TRUSTED.   Author: $author "
     exit 0
 fi
-echo "[git-gpg-check] Last commit is NOT trusted..."
+echo "[git-gpg-check] Last commit is NOT TRUSTED.   Author: $author"
 exit 1
