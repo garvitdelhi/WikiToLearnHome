ba29e49180221acb72f9890464e187bad845567d
Dario Mapelli <mapelli.dario@gmail.com>
2016-04-06 02:16:29 +0200

    added script to generate mediawiki files, to be imported on meta. added references to study how to make man pages, in ToDoList.mdown (ok, we need a phabricator). couple of minor fixes
    Signed-off-by: Dario Mapelli <mapelli.dario@gmail.com>

---
+++ doc/mediawiki-online.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# simple script that converts the markdown documentation pages to mediawiki,
+# that are then imported into meta.wikitolearn.org
+
+# set the name of the folder containing the mediawiki exported files
+mediawiki_page_folder="mediawiki-online.d"
+
+case $1 in
+    make)
+        # if the folder is not present, it creates it and creates the .placeholder file
+        # needed not to have git ignore the empty folder
+        if [[ ! -d $mediawiki_page_folder ]] ; then
+            mkdir $mediawiki_page_folder
+            touch $mediawiki_page_folder/.placeholder
+        fi
+
+        for i in *.mdown ; do
+            pandoc -r markdown -t mediawiki -o $mediawiki_page_folder/${i%.*}.mwiki $i
+            echo "Converted: $i"
+        done
+    ;;
+    clean)
+        rm -rfv $mediawiki_page_folder/*.mwiki
+    ;;
+    *)
+        echo "command not found"
+        exit
+    ;;
+esac
