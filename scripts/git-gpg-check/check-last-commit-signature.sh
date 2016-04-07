919cfe5a9e91348b582eb2bd5ade80cc66cdd1f8
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-04-07 23:17:52 +0200

    Added script to check tags. Added single commit check
    Signed-off-by: Davide Valsecchi <valsecchi.davide94@gmail.com>

--- scripts/git-gpg-check/check-last-commit-signature.sh
+++ scripts/git-gpg-check/check-last-commit-signature.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 #
-# This script checks the signature of the last commit.
+# This script checks the signature of the commit passed as
+# argument. If no commit is passed, HEAD is used.
 # If the gpg signature is trusted (inside gpg keyring)
 # the script prints the name of the author and returns 0.
 ##
 
-curr=$(pwd) #saving current directory
 #moving to WTL repo
 cd $WTL_REPO_DIR
-#reading author of last commit
-author=$(git log --pretty="format: | %h | %an | %ae"  HEAD~1..HEAD)
-#checking signature
-trusted=$(git log --pretty="format:%G?"  HEAD~1..HEAD)
-#returning to previous directory
-cd $curr
-
-# grep will exit with a non-zero status if no matches are found, which we
-# consider a success, so invert it
-if [[ $trusted == 'G' ]]; then
-    echo "[git-gpg-check] Last commit is TRUSTED $author "
-    exit 0
+COMMIT=$1
+if [[ $COMMIT == "" ]]; then
+    COMMIT="HEAD"
 fi
-echo "[git-gpg-check] Last commit is NOT TRUSTED $author"
-exit 1
+#reading author of last commit
+author=$(git show --pretty="Author: %h | %an | %ae" $COMMIT | grep Author)
+#checking signature
+git verify-commit $COMMIT &> /dev/null
+trusted=$?
+
+# if trusted is = 0 then the commit is trusted
+if [[ $trusted -gt 0 ]]; then
+    echo "[git-gpg-check] Last commit is NOT TRUSTED.    $author "
+    exit 1
+fi
+echo "[git-gpg-check] Last commit is TRUSTED.    $author"
+exit 0
