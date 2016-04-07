fe16e0c26b96d520b54f69d882b8e5bc97a3c5bd
Toma Luca <toma.luca95@gmail.com>
2016-04-07 19:56:10 +0200

    Added backup-do-quick procedure Make the backup without put in read only the system
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

---
+++ scripts/backup-do-quick.sh
@@ -0,0 +1,23 @@
+t#!/bin/bash
+[[  "$WTL_SCRIPT_DEBUG" == "1" ]] && set -x
+set -e
+if [[ $(basename $0) != "backup-do-quick.sh" ]] ; then
+    echo "Wrong way to execute backup-do-quick.sh"
+    exit 1
+fi
+cd $(dirname $(realpath $0))"/.."
+if [[ ! -f "const.sh" ]] ; then
+    echo "Error changing directory"
+    exit 1
+fi
+
+. ./load-libs.sh
+
+. $WTL_SCRIPTS/environments/${WTL_ENV}.sh
+
+if [[ "$WTL_INSTANCE_NAME" == "" ]] ; then
+    wtl-log backup-do-quick.sh 0 MIGGING_WTL_INSTANCE_NAME "You need the WTL_INSTANCE_NAME env"
+    exit 1
+fi
+
+$WTL_SCRIPTS/helpers/backup-do-quick/${WTL_HELPER_DO_BACKUP_QUICK}.sh
