7845a4aa3562dfd73b57278bbded2e49117994c0
Toma Luca <toma.luca95@gmail.com>
2016-04-09 21:30:36 +0200

    support for mw-dumps in production env

---
+++ scripts/mw-dumps-do-production.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+[[  "$WTL_SCRIPT_DEBUG" == "1" ]] && set -x
+set -e
+if [[ $(basename $0) != "mw-dumps-do-production.sh" ]] ; then
+    echo "Wrong way to execute mw-dumps-do-production.sh"
+    exit 1
+fi
+cd $(dirname $(realpath $0))"/.."
+if [[ ! -f "const.sh" ]] ; then
+    echo "Error changing directory"
+    exit 1
+fi
+
+. ./load-libs.sh
+
+. $WTL_SCRIPTS/environments/${WTL_ENV}.sh
+
+if docker inspect wikitolearn-haproxy &> /dev/null ; then
+    export WTL_INSTANCE_NAME=$(docker inspect -f '{{index .Config.Labels "WTL_INSTANCE_NAME"}}' wikitolearn-haproxy)
+    export WTL_WORKING_DIR=$(docker inspect -f '{{index .Config.Labels "WTL_WORKING_DIR"}}' wikitolearn-haproxy)
+
+    $WTL_SCRIPTS/mw-dumps-do.sh
+else
+    wtl-log mw-dumps-do-production.sh 0 MISSING_HA_PROXY "Production backup-do: missing docker wikitolearn-haproxy"
+fi
