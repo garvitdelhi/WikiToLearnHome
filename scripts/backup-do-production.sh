93d4c22c596f2a6c86426b0bd2c91f803e89f14e
Toma Luca <toma.luca95@gmail.com>
2016-04-09 17:28:45 +0200

    added script to backup automaticaly last running instance

---
+++ scripts/backup-do-production.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+[[  "$WTL_SCRIPT_DEBUG" == "1" ]] && set -x
+set -e
+if [[ $(basename $0) != "backup-do-production.sh" ]] ; then
+    echo "Wrong way to execute backup-do-production.sh"
+    exit 1
+fi
+cd $(dirname $(realpath $0))"/.."
+if [[ ! -f "const.sh" ]] ; then
+    echo "Error changing directory"
+    exit 1
+fi
+
+. ./load-libs.sh
+
+. $WTL_SCRIPTS/environments/${WTL_ENV}.sh
+
+if docker inspect wikitolearn-haproxy &> /dev/null ; then
+    export WTL_INSTANCE_NAME=$(docker inspect -f '{{index .Config.Labels "WTL_INSTANCE_NAME"}}' wikitolearn-haproxy)
+    export WTL_WORKING_DIR=$(docker inspect -f '{{index .Config.Labels "WTL_WORKING_DIR"}}' wikitolearn-haproxy)
+
+    $WTL_SCRIPTS/backup-do.sh
+fi
