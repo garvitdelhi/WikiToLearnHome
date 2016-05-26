57df4f318cd2328caf869d5a015869dcc9a7a538
Dario Mapelli <mapelli.dario@gmail.com>
2016-05-26 12:06:05 +0200

    instance.sh: added command for complete instance update (WTL, MediaWiki, docker images/containers). new order in parameter inside the case, they follow the instance-doc rationale
    Signed-off-by: Dario Mapelli <mapelli.dario@gmail.com>

--- instance.sh
+++ instance.sh
@@ -32,15 +32,8 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 case $1 in
-    first-run)
-        $WTL_SCRIPTS/download-mediawiki-extensions.sh
-        $WTL_SCRIPTS/download-all.sh
-        $WTL_SCRIPTS/create.sh
-        $WTL_SCRIPTS/start.sh
-        $WTL_SCRIPTS/backup-restore.sh $WTL_REPO_DIR/DeveloperDump/
-        $WTL_SCRIPTS/unuse-instance.sh
-        $WTL_SCRIPTS/use-instance.sh
-        $WTL_SCRIPTS/update-db.sh
+    help)
+        cat doc/wtlh-user-guide/instance-doc.mdown
     ;;
     create)
         $WTL_SCRIPTS/create.sh
@@ -51,14 +44,27 @@ case $1 in
         $WTL_SCRIPTS/use-instance.sh
         $WTL_SCRIPTS/fix-hosts.sh
     ;;
-    stop)
-        $WTL_SCRIPTS/stop.sh
+    first-run)
+        $WTL_SCRIPTS/download-mediawiki-extensions.sh
+        $WTL_SCRIPTS/download-all.sh
+        $WTL_SCRIPTS/create.sh
+        $WTL_SCRIPTS/start.sh
+        $WTL_SCRIPTS/backup-restore.sh $WTL_REPO_DIR/DeveloperDump/
         $WTL_SCRIPTS/unuse-instance.sh
+        $WTL_SCRIPTS/use-instance.sh
+        $WTL_SCRIPTS/update-db.sh
+    ;;
+    stop)
+        $WTL_SCRIPTS/unuse-instance.sh
+        $WTL_SCRIPTS/stop.sh
     ;;
     delete)
         $WTL_SCRIPTS/unuse-instance.sh
         $WTL_SCRIPTS/stop.sh
         $WTL_SCRIPTS/delete.sh
+    ;;
+    delete-volumes)
+        $WTL_SCRIPTS/delete-volumes.sh
     ;;
     delete-full)
         $WTL_SCRIPTS/unuse-instance.sh
@@ -66,8 +72,15 @@ case $1 in
         $WTL_SCRIPTS/delete.sh
         $WTL_SCRIPTS/delete-volumes.sh
     ;;
-    delete-volumes)
-        $WTL_SCRIPTS/delete-volumes.sh
+    fix-hosts)
+        $WTL_SCRIPTS/fix-hosts.sh
+    ;;
+    update-home)
+        $WTL_SCRIPTS/update-home.sh
+    ;;
+    download)
+        $WTL_SCRIPTS/download-code.sh
+        $WTL_SCRIPTS/download-mediawiki-extensions.sh
     ;;
     update-docker-container)
         $WTL_SCRIPTS/pull-images.sh
@@ -79,6 +92,19 @@ case $1 in
         $WTL_SCRIPTS/use-instance.sh
         $WTL_SCRIPTS/fix-hosts.sh
     ;;
+    update-code-and-db)
+        $WTL_SCRIPTS/download-code.sh
+        $WTL_SCRIPTS/download-mediawiki-extensions.sh
+        $WTL_SCRIPTS/pull-images.sh
+        $WTL_SCRIPTS/unuse-instance.sh
+        $WTL_SCRIPTS/stop.sh
+        $WTL_SCRIPTS/delete.sh
+        $WTL_SCRIPTS/create.sh
+        $WTL_SCRIPTS/start.sh
+        $WTL_SCRIPTS/use-instance.sh
+        $WTL_SCRIPTS/fix-hosts.sh
+        $WTL_SCRIPTS/update-db.sh
+    ;;
     devdump-import)
         $WTL_SCRIPTS/backup-restore.sh $WTL_REPO_DIR/DeveloperDump/
         $WTL_SCRIPTS/update-db.sh
@@ -86,23 +112,6 @@ case $1 in
     devdump-export)
         $WTL_SCRIPTS/backup-do.sh
         $WTL_SCRIPTS/copy-last-backup-to-devdump.sh
-    ;;
-    download)
-        $WTL_SCRIPTS/download-code.sh
-        $WTL_SCRIPTS/download-mediawiki-extensions.sh
-    ;;
-    fix-hosts)
-        $WTL_SCRIPTS/fix-hosts.sh
-    ;;
-    update-home)
-        $WTL_SCRIPTS/update-home.sh
-    ;;
-    staging)
-        $WTL_SCRIPTS/relase-procedure.sh
-        $WTL_SCRIPTS/backup-auto-delete.sh
-        $WTL_SCRIPTS/unused-instance-stop-delete.sh
-        $WTL_SCRIPTS/docker-images-delete-old-images.sh
-        $WTL_SCRIPTS/docker-images-clean.sh
     ;;
     release-do)
         $WTL_SCRIPTS/relase-procedure.sh
@@ -113,8 +122,12 @@ case $1 in
         $WTL_SCRIPTS/docker-images-delete-old-images.sh
         $WTL_SCRIPTS/docker-images-clean.sh
     ;;
-    help)
-        cat doc/wtlh-user-guide/instance-doc.mdown
+    staging)
+        $WTL_SCRIPTS/relase-procedure.sh
+        $WTL_SCRIPTS/backup-auto-delete.sh
+        $WTL_SCRIPTS/unused-instance-stop-delete.sh
+        $WTL_SCRIPTS/docker-images-delete-old-images.sh
+        $WTL_SCRIPTS/docker-images-clean.sh
     ;;
     *)
         echo "Command not found ($@)"
