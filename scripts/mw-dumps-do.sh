c93a640192b84249051b662a478503af45aae510
Toma Luca <toma.luca95@gmail.com>
2016-05-30 01:19:47 +0200

    added support for multiple dumps (the id of the dump is the date, like for bakcups)
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- scripts/mw-dumps-do.sh
+++ scripts/mw-dumps-do.sh
@@ -19,8 +19,13 @@ if [[ "$WTL_INSTANCE_NAME" == "" ]] ; then
     wtl-log mw-dumps-do.sh 0 MIGGING_WTL_INSTANCE_NAME "You need the WTL_INSTANCE_NAME env"
     exit 1
 fi
+CURRENT_DUMP_DIR=$(date +'%Y_%m_%d__%H_%M_%S')
+if ! test -d $WTL_MW_DUMPS"/"$CURRENT_DUMP_DIR"/" ; then
+    mkdir $WTL_MW_DUMPS"/"$CURRENT_DUMP_DIR"/"
+fi
 for db in $(docker exec -ti ${WTL_INSTANCE_NAME}-mysql mysql -e "SHOW DATABASES" | grep wikitolearn | awk '{ print $2 }') ; do
     SUBDOM=$(echo $db | sed 's/wikitolearn//g')
     wtl-log mw-dumps-do.sh 0 DUMPING "Dump for $SUBDOM.$WTL_DOMAIN_NAME"
-    docker exec -ti ${WTL_INSTANCE_NAME}-websrv sh -c "export WIKI='$SUBDOM.$WTL_DOMAIN_NAME' && pwd && cd /var/www/WikiToLearn/mediawiki/maintenance/ && php dumpBackup.php --full" > $WTL_MW_DUMPS"/"$SUBDOM.xml
+    docker exec -ti ${WTL_INSTANCE_NAME}-websrv sh -c "export WIKI='$SUBDOM.$WTL_DOMAIN_NAME' && pwd && cd /var/www/WikiToLearn/mediawiki/maintenance/ && php dumpBackup.php --full" > $WTL_MW_DUMPS"/"$CURRENT_DUMP_DIR"/"$SUBDOM.xml
 done
+wtl-log mw-dumps-do.sh 0 DUMP_END "Dump finished"
