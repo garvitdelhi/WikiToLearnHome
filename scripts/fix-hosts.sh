a79d50f875f5ed7fe65576370231535b882985cd
Toma Luca <toma.luca95@gmail.com>
2016-09-19 15:29:09 +0200

    changed fix-hosts to not use an array but use a string (wtl-event issue)
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- scripts/fix-hosts.sh
+++ scripts/fix-hosts.sh
@@ -18,17 +18,18 @@ fi
 
 WEBSRV_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" ${WTL_INSTANCE_NAME}-websrv)
 
-web_hosts=($(for subdom in $(cat $WTL_REPO_DIR/databases.conf | sed 's/wikitolearn//g' | grep -v shared); do
-  echo "${subdom}.$WTL_DOMAIN_NAME "
-done))
+web_hosts=""
+for subdom in $(cat $WTL_REPO_DIR/databases.conf | sed 's/wikitolearn//g' | grep -v shared); do
+  web_hosts=$web_hosts" ${subdom}.$WTL_DOMAIN_NAME"
+done
 
 for d in ${WTL_INSTANCE_NAME}-{parsoid,ocg,restbase} ; do
     docker inspect $d &> /dev/null && {
-        wtl-event FIX_HOSTS_DO_IP $d "${web_hosts[@]}" $WEBSRV_IP
+        wtl-event FIX_HOSTS_DO_IP $d "$web_hosts" $WEBSRV_IP
         {
             docker exec $d sed '/FIXHOSTWEBSRV/d' /etc/hosts | docker exec -i $d tee /tmp/tmp_hosts
             docker exec $d cat /tmp/tmp_hosts | docker exec -i $d tee /etc/hosts
-            echo $WEBSRV_IP" FIXHOSTWEBSRV ${web_hosts[@]}" | docker exec -i $d tee -a /etc/hosts
+            echo $WEBSRV_IP" FIXHOSTWEBSRV $web_hosts" | docker exec -i $d tee -a /etc/hosts
         } &> /dev/null
     }
 done
