5593a93ec212e4a8419e72cf6761a31337df9195
Toma Luca <toma.luca95@gmail.com>
2016-09-25 19:20:38 +0200

    more work on the new wtl-event system
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- scripts/helpers/create/commons/single-node-pre.sh
+++ scripts/helpers/create/commons/single-node-pre.sh
@@ -4,54 +4,54 @@
 #Control the existance of docker instances and create them if they are not existing
 if ! docker inspect ${WTL_INSTANCE_NAME}-parsoid &> /dev/null ; then
     docker create -ti $MORE_ARGS --hostname parsoid --name ${WTL_INSTANCE_NAME}-parsoid $WTL_DOCKER_PARSOID
-    wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Creating docker ${WTL_INSTANCE_NAME}-parsoid"
+    wtl-event CREATE_DOCKER_PARSOID ${WTL_INSTANCE_NAME}
 fi
 
 if [[ "$WTL_MATHOID_NUM_WORKERS" == "" ]] ; then
-    wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "Using the default WTL_MATHOID_NUM_WORKERS value (is 1)"
+    wtl-event MATHOID_NUM_WORKERS_DEFAULT_VALUE
     export WTL_MATHOID_NUM_WORKERS=1
 else
-    wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "Using "$WTL_MATHOID_NUM_WORKERS" mathoid workers"
+    wtl-event MATHOID_NUM_WORKERS_CONF_VALUE $WTL_MATHOID_NUM_WORKERS
 fi
 
 if [[ "$WTL_RESTBASE_CASSANDRA_HOSTS" == "" ]] ; then
-    wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "Using sqlite as restbase backend"
+    wtl-event RESTBASE_CASSANDRA_HOSTS_EMPTY
     export WTL_RESTBASE_CASSANDRA_HOSTS=""
 fi
 
 if ! docker inspect ${WTL_INSTANCE_NAME}-mathoid &> /dev/null ; then
     docker create -ti $MORE_ARGS --hostname mathoid --name ${WTL_INSTANCE_NAME}-mathoid -e NUM_WORKERS=$WTL_MATHOID_NUM_WORKERS $WTL_DOCKER_MATHOID
-    wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Creating docker ${WTL_INSTANCE_NAME}-mathoid with $WTL_MATHOID_NUM_WORKERS workers"
+    wtl-event CREATE_DOCKER_MATHOID ${WTL_INSTANCE_NAME} $WTL_MATHOID_NUM_WORKERS
 fi
 
 
 if ! docker inspect ${WTL_INSTANCE_NAME}-memcached &> /dev/null ; then
     docker create -ti $MORE_ARGS --hostname memcached --name ${WTL_INSTANCE_NAME}-memcached $WTL_DOCKER_MEMCACHED
-    wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Creating docker ${WTL_INSTANCE_NAME}-memcached"
+    wtl-event CREATE_DOCKER_MEMCACHED ${WTL_INSTANCE_NAME}
 fi
 
 
 if ! docker inspect ${WTL_INSTANCE_NAME}-mysql &> /dev/null ; then
     if [[ -f $WTL_CONFIGS_DIR/mysql-users/root ]] ; then
         ROOT_PWD=$(cat $WTL_CONFIGS_DIR/mysql-users/root)
-        wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Using existing mysql root passwd"
+        wtl-event MYSQL_OLD_ROOT_PASSWORD
     else
         ROOT_PWD=$(echo $RANDOM$RANDOM$(date +%s) | sha256sum | base64 | head -c 32 )
-        wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Using new mysql root passwd"
+        wtl-event MYSQL_NEW_ROOT_PASSWORD
         echo $ROOT_PWD > $WTL_CONFIGS_DIR/mysql-users/root
     fi
 
     if [[ -f $WTL_CONFIGS_DIR/mysql-users/wtlmysql ]] ; then
         WTLMYSQL_PWD=$(cat $WTL_CONFIGS_DIR/mysql-users/wtlmysql)
-        wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Using existing mysql wtlmysql passwd"
+        wtl-event MYSQL_OLD_USER_PASSWORD
     else
         WTLMYSQL_PWD=$(echo $RANDOM$RANDOM$(date +%s) | sha256sum | base64 | head -c 32 )
-        wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Using new mysql wtlmysql passwd"
+        wtl-event MYSQL_NEW_USER_PASSWORD
         echo $WTLMYSQL_PWD > $WTL_CONFIGS_DIR/mysql-users/wtlmysql
     fi
 
     docker create -ti $MORE_ARGS -v ${WTL_VOLUME_DIR}${WTL_INSTANCE_NAME}-var-lib-mysql:/var/lib/mysql --hostname mysql --name ${WTL_INSTANCE_NAME}-mysql -e MYSQL_ROOT_PASSWORD=$ROOT_PWD $WTL_DOCKER_MYSQL
-    wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Creating docker ${WTL_INSTANCE_NAME}-mysql"
+    wtl-event CREATE_DOCKER_MYSQL ${WTL_INSTANCE_NAME}
 
     echo "[client]" > $WTL_CONFIGS_DIR/mysql-root-password.cnf
     echo "user=root" >> $WTL_CONFIGS_DIR/mysql-root-password.cnf
@@ -65,5 +65,5 @@ if ! docker inspect ${WTL_INSTANCE_NAME}-restbase &> /dev/null ; then
         --link ${WTL_INSTANCE_NAME}-parsoid:parsoid \
         --link ${WTL_INSTANCE_NAME}-mathoid:mathoid \
         --name ${WTL_INSTANCE_NAME}-restbase $WTL_DOCKER_RESTBASE
-    wtl-log scripts/helpers/create/commons/single-node-pre.sh 7 NN "[create/single-node] Creating docker ${WTL_INSTANCE_NAME}-restbase"
+    wtl-event CREATE_DOCKER_RESTBASE ${WTL_INSTANCE_NAME}
 fi
