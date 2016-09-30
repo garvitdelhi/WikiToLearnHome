ff186596fe5b910eaf918a85adc325e9f79ea76d
Cristian Baldi <bld.cris.96@gmail.com>
2016-09-30 10:42:10 +0200

    Fix key trusting

--- scripts/git-gpg-check/update-trusted-keys.sh
+++ scripts/git-gpg-check/update-trusted-keys.sh
@@ -29,9 +29,11 @@ then
     fi
 
     for f in $(find $WTL_TRUSTED_KEYS_REPO -name '*.key'); do
-        key=$(gpg $f | grep pub | awk -F '/' '{print $2}')
-        key=${key:0:9}
-        gpg --import $f &> /dev/null
+        gpg --import $f
+
+        key=$(gpg --with-fingerprint $f | grep ^pub -A1 | tail -n1 | tr -d '[:space:]')
+        echo "$key:6:" | gpg --import-ownertrust
+
         #saving the trusted key in config file
         echo $key >> $WTL_TRUSTED_KEYS_INDEX
     done
