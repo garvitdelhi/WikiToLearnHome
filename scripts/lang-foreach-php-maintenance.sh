4b6e1eaf6c387d47fde1643e6a7980ec68a07c0b
Toma Luca <toma.luca95@gmail.com>
2016-10-05 22:30:04 +0200

    using $WTL_DOMAIN_NAME in lang-foreach-php-maintenance.sh instade of wikitolearn.org
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- scripts/lang-foreach-php-maintenance.sh
+++ scripts/lang-foreach-php-maintenance.sh
@@ -34,5 +34,5 @@ docker exec ${WTL_INSTANCE_NAME}-websrv chown www-data: /var/www/
 
 for lang in $langlist; do
     wtl-event LANG_FOREACH_PHP_MAINTENANCE_RUN_LANG $lang "$CMD"
-    docker exec -ti ${WTL_INSTANCE_NAME}-websrv su -s /bin/sh -c "/bin/bash -c 'WIKI=$lang.wikitolearn.org php /var/www/WikiToLearn/mediawiki/maintenance/$CMD'" www-data
+    docker exec -ti ${WTL_INSTANCE_NAME}-websrv su -s /bin/sh -c "/bin/bash -c 'WIKI=$lang.$WTL_DOMAIN_NAME php /var/www/WikiToLearn/mediawiki/maintenance/$CMD'" www-data
 done
